a77c20ece537b6af183e2c2dc2a40b5057a126ea
Tuba <76249160+asttubi@users.noreply.github.com>
2021-03-24 13:08:02 +0300

    Update PythonTemelProje1.py

--- PythonTemelProje1.py
+++ PythonTemelProje1.py
@@ -1,3 +1,6 @@
+#Bir listeyi düzleştiren (flatten) fonksiyon yazın. Elemanları birden çok katmanlı listtlerden ([[3],2] gibi) oluşabileceği gibi, non-scalar verilerden de oluşabilir.
+
+
 def flatten(list_of_lists):
     if len(list_of_lists) == 0:
         return list_of_lists
@@ -5,4 +8,4 @@ def flatten(list_of_lists):
         return flatten(list_of_lists[0]) + flatten(list_of_lists[1:])
     return list_of_lists[:1] + flatten(list_of_lists[1:])
 
-print(flatten([[[1, "a",["dog"], 9], [[[3]]], "dog"], 4, 10]))
+print(flatten([[1,'a',['cat'],2],[[[3]],'dog'],4,5]))
